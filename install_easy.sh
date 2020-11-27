0f41cd30a1544bcdef09e7be18e4bcc067a0d6b4
bol-van <k@vodka.home.kg>
2020-11-27 10:41:29 +0300

    install_easy: DNS check russia targeted notice

--- install_easy.sh
+++ install_easy.sh
@@ -743,7 +743,7 @@ check_dns()
 	}
 	echo all resolved IPs are unique
 	echo -- DNS looks good
-	echo -- NOTE this check is Russia targeted. In your country other domains can blocked.
+	echo -- NOTE this check is Russia targeted. In your country other domains can be blocked.
 	check_dns_cleanup
 	true
 }
